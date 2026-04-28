ca3d2e6f6f4be6f6d7dbd75c06e49ebea148d6d1
Shedrack Nliam <shedracknliam@gmail.com>
2026-04-28 14:35:44 +0100

    fix: enable global API prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api/v1'); // 🔥 ADD THIS
+
   app.enableCors({
     origin: [
       'http://localhost:3000',
